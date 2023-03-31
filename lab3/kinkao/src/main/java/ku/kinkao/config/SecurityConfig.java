8f7f2ca329f61f07cc62b812f7221bcc1c825472
Chatchawarin Chatchavalwong <numfahmaek@gmail.com>
2023-03-31 21:12:21 +0700

    Use environment variables in place of sensitive data

---
+++ lab3/kinkao/src/main/java/ku/kinkao/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package ku.kinkao.config;
+
+import ku.kinkao.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+   @Autowired
+   private UserDetailsServiceImp userDetailsService;
+
+   @Autowired
+   private OidcUserService oidcUserService;
+
+   @Autowired
+   private ApplicationContext context;
+
+
+
+   @Bean
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+       http
+               .authorizeRequests()
+               .antMatchers("/home", "/signup", 
+                            "/css/**", "/js/**").permitAll()
+               .anyRequest().authenticated()
+
+       .and()
+               .formLogin()
+               .loginPage("/login")
+               .defaultSuccessUrl("/restaurant", true)
+               .permitAll()
+        .and()
+               .oauth2Login()
+               .defaultSuccessUrl("/restaurant", true)
+       .and()
+               .logout()
+               .logoutUrl("/logout")
+               .clearAuthentication(true)
+               .invalidateHttpSession(true)
+               .deleteCookies("JSESSIONID", "remember-me")
+               .permitAll();
+
+        ClientRegistrationRepository repository =
+                context.getBean(ClientRegistrationRepository.class);
+
+       if (repository != null) {
+           http
+                  .oauth2Login().clientRegistrationRepository(repository)
+                  .userInfoEndpoint().oidcUserService(oidcUserService).and()
+                  .loginPage("/login").permitAll();
+       }
+      return http.build();
+   }
+
+   @Bean
+   public PasswordEncoder encoder() {
+       return new BCryptPasswordEncoder(12);
+   }
+}
---
+++ lab3/kinkao/src/main/java/ku/kinkao/controller/RestaurantController.java
@@ -0,0 +1,35 @@
+package ku.kinkao.controller;
+
+import ku.kinkao.model.Restaurant;
+import ku.kinkao.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RestaurantController {
+
+   @Autowired
+   private RestaurantRepository repository;
+
+   @GetMapping("/restaurant")
+   public String getRestaurantPage(Model model) {
+       model.addAttribute("restaurants", repository.findAll());
+       return "restaurant";
+   }
+
+   @GetMapping("/restaurant/add")
+   public String getAddPage() {
+       return "restaurant-add";  // return restaurant-add.html
+   }
+
+   @PostMapping("/restaurant/add")
+   public String addRestaurant(@ModelAttribute Restaurant restaurant, 
+                               Model model) {
+       repository.save(restaurant);
+       return "redirect:/restaurant";
+   }
+}
