2f564ee1426728b281d382c6b71ef1bb6427c13f
Chatchawarin Chatchavalwong <numfahmaek@gmail.com>
2023-04-06 01:16:40 +0700

    init lab4

---
+++ lab4/kinkao/src/main/java/ku/kinkao/controller/RestaurantController.java
@@ -0,0 +1,35 @@
+package ku.kinkao.controller;
+
+import ku.kinkao.dto.RestaurantDto;
+import ku.kinkao.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RestaurantController {
+
+   @Autowired
+   private RestaurantService restaurantService;
+
+   @GetMapping("/restaurant")
+   public String getRestaurantPage(Model model) {
+       model.addAttribute("restaurants", restaurantService.getRestaurants());
+       return "restaurant";
+   }
+
+   @GetMapping("/restaurant/add")
+   public String getAddPage() {
+       return "restaurant-add";  // return restaurant-add.html
+   }
+   
+   @PostMapping("/restaurant/add")
+   public String addRestaurant(@ModelAttribute RestaurantDto restaurant,
+                               Model model) {
+       restaurantService.create(restaurant);
+       return "redirect:/restaurant";
+   }
+}
---
+++ lab4/kinkao/src/main/java/ku/kinkao/model/Member.java
@@ -0,0 +1,25 @@
+package ku.kinkao.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Member {
+
+   @Id
+   @GeneratedValue
+   private UUID id;
+
+   private String username;
+   private String password;
+   private String firstName;
+   private String lastName;
+   private Instant createdAt;
+}
