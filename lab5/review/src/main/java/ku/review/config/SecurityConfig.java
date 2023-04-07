cf07832ed8893d05b6afdb472024a7cd057526e5
Chatchawarin Chatchavalwong <numfahmaek@gmail.com>
2023-04-07 23:27:52 +0700

    :truck: try change branch name :truck:

--- lab5/review/src/main/java/ku/review/config/SecurityConfig.java
+++ lab5/review/src/main/java/ku/review/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ku.review.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
