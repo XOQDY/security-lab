a22554513a87daf2b1d93c8844995c42518a70ad
Chatchawarin Chatchavalwong <numfahmaek@gmail.com>
2023-04-12 00:05:42 +0700

    init lab7

---
+++ lab7/kinkao/src/main/java/ku/kinkao/config/ComponentConfig.java
@@ -0,0 +1,19 @@
+package ku.kinkao.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ComponentConfig {
+
+   @Bean
+   public ModelMapper modelMapper() {
+       return new ModelMapper();
+   }
+
+   @Bean
+   public RestTemplate restTemplate(RestTemplateBuilder builder) {
+       return builder.build();
+   }
+}
