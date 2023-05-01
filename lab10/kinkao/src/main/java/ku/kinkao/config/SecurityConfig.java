a3460d64c89c33c048a34f3d5c9cff73eb5aee30
Chatchawarin Chatchavalwong <numfahmaek@gmail.com>
2023-05-01 21:31:01 +0700

    init lab10

---
+++ lab10/kinkao/src/main/java/ku/kinkao/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package ku.kinkao.config;
+
+import ku.kinkao.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+   @Autowired
+   private UserDetailsServiceImp userDetailsService;
+
+   @Autowired
+   private OidcUserService oidcUserService;
+
+   @Autowired
+   private ApplicationContext context;
+
+
+
+   @Bean
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+       http
+               .authorizeRequests()
+               .mvcMatchers(HttpMethod.GET, "/api/review")
+               .hasAuthority("SCOPE_read:reviews")
+               .mvcMatchers(HttpMethod.POST, "/api/review")
+               .hasAuthority("SCOPE_create:reviews")
+               .anyRequest()
+               .authenticated()
+       .and()
+
+
+               .formLogin()
+               .loginPage("/login")
+               .defaultSuccessUrl("/restaurant", true)
+               .permitAll()
+        .and()
+               .oauth2Login()
+               .defaultSuccessUrl("/restaurant", true)
+       .and()
+               .logout()
+               .logoutUrl("/logout")
+               .clearAuthentication(true)
+               .invalidateHttpSession(true)
+               .deleteCookies("JSESSIONID", "remember-me")
+               .permitAll();
+
+        ClientRegistrationRepository repository =
+                context.getBean(ClientRegistrationRepository.class);
+
+       if (repository != null) {
+           http
+                  .oauth2Login().clientRegistrationRepository(repository)
+                  .userInfoEndpoint().oidcUserService(oidcUserService).and()
+                  .loginPage("/login").permitAll();
+       }
+      return http.build();
+   }
+
+   @Bean
+   public PasswordEncoder encoder() {
+       return new BCryptPasswordEncoder(12);
+   }
+   @Override
+   public void configure(WebSecurity web) throws Exception {
+       web
+               .ignoring()
+               .antMatchers("/h2-console/**");
+   }
+}
---
+++ lab10/kinkao/src/main/java/ku/kinkao/controller/RestaurantController.java
@@ -0,0 +1,41 @@
+package ku.kinkao.controller;
+
+import ku.kinkao.dto.RestaurantDto;
+import ku.kinkao.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+
+@Controller
+public class RestaurantController {
+
+   @Autowired
+   private RestaurantService restaurantService;
+
+   @GetMapping("/restaurant")
+   public String getRestaurantPage(Model model) {
+       model.addAttribute("restaurants", restaurantService.getRestaurants());
+       return "restaurant";
+   }
+
+   @GetMapping("/restaurant/add")
+   public String getAddPage(RestaurantRequest restaurantRequest) {
+       return "restaurant-add";  // return restaurant-add.html
+   }
+
+   @PostMapping("/restaurant/add")
+   public String addRestaurant(@Valid RestaurantRequest restaurant,
+                               BindingResult result,
+                               Model model) {
+       if (result.hasErrors())
+           return "restaurant-add";
+
+       restaurantService.create(restaurant);
+       return "redirect:/restaurant";
+   }
+}
