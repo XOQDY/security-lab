936047f4d9f60bfc7dbc8313f1e17f00361b6b51
Chatchawarin Chatchavalwong <numfahmaek@gmail.com>
2023-05-01 22:02:23 +0700

    fix bug merge lab1

---
+++ lab1/kinkao/src/main/java/ku/kinkao/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package ku.kinkao.config;
+
+import ku.kinkao.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+   @Autowired
+   private UserDetailsServiceImp userDetailsService;
+
+
+   @Bean
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+       http
+               .authorizeRequests()
+               .antMatchers("/home", "/signup", 
+                            "/css/**", "/js/**").permitAll()
+               .anyRequest().authenticated()
+
+       .and()
+               .formLogin()
+               .defaultSuccessUrl("/restaurant", true)
+       .and()
+               .logout();
+
+
+      return http.build();
+   }
+
+   @Bean
+   public PasswordEncoder encoder() {
+       return new BCryptPasswordEncoder(12);
+   }
+}
